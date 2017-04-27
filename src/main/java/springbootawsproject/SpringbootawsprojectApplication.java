a00e51e2029214b5bccb7d72ad9c8b405818713e
feixinghui <hfhelena723@gmail.com>
2017-04-27 16:15:21 -0600

    Add bootstrap and jquery to pom

---
+++ src/main/java/springbootawsproject/SpringbootawsprojectApplication.java
@@ -0,0 +1,12 @@
+package springbootawsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootawsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootawsprojectApplication.class, args);
+	}
+}
